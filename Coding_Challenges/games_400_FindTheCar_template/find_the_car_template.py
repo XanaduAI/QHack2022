8da235bdb8254bd2b4d3b9105aae5053491af2f3
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:29:37 +0000

    Copied games_400_FindTheCar problem templates

---
+++ Coding_Challenges/games_400_FindTheCar_template/find_the_car_template.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+
+dev = qml.device("default.qubit", wires=[0, 1, "sol"], shots=1)
+
+
+def find_the_car(oracle):
+    """Function which, given an oracle, returns which door that the car is behind.
+
+    Args:
+        - oracle (function): function that will act as an oracle. The first two qubits (0,1)
+        will refer to the door and the third ("sol") to the answer.
+
+    Returns:
+        - (int): 0, 1, 2, or 3. The door that the car is behind.
+    """
+
+    @qml.qnode(dev)
+    def circuit1():
+        # QHACK #
+
+        # QHACK #
+        return qml.sample()
+
+    @qml.qnode(dev)
+    def circuit2():
+        # QHACK #
+
+        # QHACK #
+        return qml.sample()
+
+    sol1 = circuit1()
+    sol2 = circuit2()
+
+    # QHACK #
+
+    # process sol1 and sol2 to determine which door the car is behind.
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    numbers = [int(i) for i in inputs]
+
+    def oracle():
+        if numbers[0] == 1:
+            qml.PauliX(wires=0)
+        if numbers[1] == 1:
+            qml.PauliX(wires=1)
+        qml.Toffoli(wires=[0, 1, "sol"])
+        if numbers[0] == 1:
+            qml.PauliX(wires=0)
+        if numbers[1] == 1:
+            qml.PauliX(wires=1)
+
+    output = find_the_car(oracle)
+    print(f"{output}")
