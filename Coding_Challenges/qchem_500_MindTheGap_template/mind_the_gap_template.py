47d663ab0551adf7bc939448ce999dcb97e5273d
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:37:22 +0000

    Copied qchem_500_MindTheGap problem templates

---
+++ Coding_Challenges/qchem_500_MindTheGap_template/mind_the_gap_template.py
@@ -0,0 +1,69 @@
+import sys
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane import hf
+
+
+def ground_state_VQE(H):
+    """Perform VQE to find the ground state of the H2 Hamiltonian.
+
+    Args:
+        - H (qml.Hamiltonian): The Hydrogen (H2) Hamiltonian
+
+    Returns:
+        - (float): The ground state energy
+        - (np.ndarray): The ground state calculated through your optimization routine
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+
+def create_H1(ground_state, beta, H):
+    """Create the H1 matrix, then use `qml.Hermitian(matrix)` to return an observable-form of H1.
+
+    Args:
+        - ground_state (np.ndarray): from the ground state VQE calculation
+        - beta (float): the prefactor for the ground state projector term
+        - H (qml.Hamiltonian): the result of hf.generate_hamiltonian(mol)()
+
+    Returns:
+        - (qml.Observable): The result of qml.Hermitian(H1_matrix)
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+
+def excited_state_VQE(H1):
+    """Perform VQE using the "excited state" Hamiltonian.
+
+    Args:
+        - H1 (qml.Observable): result of create_H1
+
+    Returns:
+        - (float): The excited state energy
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    coord = float(sys.stdin.read())
+    symbols = ["H", "H"]
+    geometry = np.array([[0.0, 0.0, -coord], [0.0, 0.0, coord]], requires_grad=False)
+    mol = hf.Molecule(symbols, geometry)
+
+    H = hf.generate_hamiltonian(mol)()
+    E0, ground_state = ground_state_VQE(H)
+
+    beta = 15.0
+    H1 = create_H1(ground_state, beta, H)
+    E1 = excited_state_VQE(H1)
+
+    answer = [np.real(E0), E1]
+    print(*answer, sep=",")
