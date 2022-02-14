3170a9c64b3bfb18c12a97dc2454d2f64aad5607
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:36:42 +0000

    Copied qchem_400_TripleGivens problem templates

---
+++ Coding_Challenges/qchem_400_TripleGivens_template/triple_givens_template.py
@@ -0,0 +1,49 @@
+import sys
+import pennylane as qml
+from pennylane import numpy as np
+
+NUM_WIRES = 6
+
+
+def triple_excitation_matrix(gamma):
+    """The matrix representation of a triple-excitation Givens rotation.
+
+    Args:
+        - gamma (float): The angle of rotation
+
+    Returns:
+        - (np.ndarray): The matrix representation of a triple-excitation
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+
+dev = qml.device("default.qubit", wires=6)
+
+
+@qml.qnode(dev)
+def circuit(angles):
+    """Prepares the quantum state in the problem statement and returns qml.probs
+
+    Args:
+        - angles (list(float)): The relevant angles in the problem statement in this order:
+        [alpha, beta, gamma]
+
+    Returns:
+        - (np.tensor): The probability of each computational basis state
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+    return qml.probs(wires=range(NUM_WIRES))
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = np.array(sys.stdin.read().split(","), dtype=float)
+    probs = circuit(inputs).round(6)
+    print(*probs, sep=",")
