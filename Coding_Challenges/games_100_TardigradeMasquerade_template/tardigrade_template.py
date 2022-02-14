0daf4f4b6753592bca71a5e45add366d05a02717
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:27:08 +0000

    Copied games_100_TardigradeMasquerade problem templates

---
+++ Coding_Challenges/games_100_TardigradeMasquerade_template/tardigrade_template.py
@@ -0,0 +1,38 @@
+import sys
+import pennylane as qml
+from pennylane import numpy as np
+
+
+def second_renyi_entropy(rho):
+    """Computes the second Renyi entropy of a given density matrix."""
+    # DO NOT MODIFY anything in this code block
+    rho_diag_2 = np.diagonal(rho) ** 2.0
+    return -np.real(np.log(np.sum(rho_diag_2)))
+
+
+def compute_entanglement(theta):
+    """Computes the second Renyi entropy of circuits with and without a tardigrade present.
+
+    Args:
+        - theta (float): the angle that defines the state psi_ABT
+
+    Returns:
+        - (float): The entanglement entropy of qubit B with no tardigrade
+        initially present
+        - (float): The entanglement entropy of qubit B where the tardigrade
+        was initially present
+    """
+
+    dev = qml.device("default.qubit", wires=3)
+
+    # QHACK #
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    theta = np.array(sys.stdin.read(), dtype=float)
+
+    S2_without_tardigrade, S2_with_tardigrade = compute_entanglement(theta)
+    print(*[S2_without_tardigrade, S2_with_tardigrade], sep=",")
