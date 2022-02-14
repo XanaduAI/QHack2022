254003247fdd76f8ac3dea44456f98759e938f7a
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:40:15 +0000

    Copied qml_400_BuildingQRAM problem templates

---
+++ Coding_Challenges/qml_400_BuildingQRAM_template/building_QRAM_template.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+
+def qRAM(thetas):
+    """Function that generates the superposition state explained above given the thetas angles.
+
+    Args:
+        - thetas (list(float)): list of angles to apply in the rotations.
+
+    Returns:
+        - (list(complex)): final state.
+    """
+
+    # QHACK #
+
+    # Use this space to create auxiliary functions if you need it.
+
+    # QHACK #
+
+    dev = qml.device("default.qubit", wires=range(4))
+
+    @qml.qnode(dev)
+    def circuit():
+
+        # QHACK #
+
+        # Create your circuit: the first three qubits will refer to the index, the fourth to the RY rotation.
+
+        # QHACK #
+
+        return qml.state()
+
+    return circuit()
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    thetas = np.array(inputs, dtype=float)
+
+    output = qRAM(thetas)
+    output = [float(i.real.round(6)) for i in output]
+    print(*output, sep=",")
