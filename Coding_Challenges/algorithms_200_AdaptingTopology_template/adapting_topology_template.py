a41f2dc856d7313361e1334a4ad4e50982bb96f0
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:24:13 +0000

    Copied algorithms_200_AdaptingTopology problem templates

---
+++ Coding_Challenges/algorithms_200_AdaptingTopology_template/adapting_topology_template.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+graph = {
+    0: [1],
+    1: [0, 2, 3, 4],
+    2: [1],
+    3: [1],
+    4: [1, 5, 7, 8],
+    5: [4, 6],
+    6: [5, 7],
+    7: [4, 6],
+    8: [4],
+}
+
+
+def n_swaps(cnot):
+    """Count the minimum number of swaps needed to create the equivalent CNOT.
+
+    Args:
+        - cnot (qml.Operation): A CNOT gate that needs to be implemented on the hardware
+        You can find out the wires on which an operator works by asking for the 'wires' attribute: 'cnot.wires'
+
+    Returns:
+        - (int): minimum number of swaps
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    output = n_swaps(qml.CNOT(wires=[int(i) for i in inputs]))
+    print(f"{output}")
