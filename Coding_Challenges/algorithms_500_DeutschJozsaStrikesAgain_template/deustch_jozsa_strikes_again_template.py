5792e391cce6e8a8e8f8c8b806b915cd349fb67c
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:26:27 +0000

    Copied algorithms_500_DeutschJozsaStrikesAgain problem templates

---
+++ Coding_Challenges/algorithms_500_DeutschJozsaStrikesAgain_template/deustch_jozsa_strikes_again_template.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+
+def deutsch_jozsa(fs):
+    """Function that determines whether four given functions are all of the same type or not.
+
+    Args:
+        - fs (list(function)): A list of 4 quantum functions. Each of them will accept a 'wires' parameter.
+        The first two wires refer to the input and the third to the output of the function.
+
+    Returns:
+        - (str) : "4 same" or "2 and 2"
+    """
+
+    # QHACK #
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    numbers = [int(i) for i in inputs]
+
+    # Definition of the four oracles we will work with.
+
+    def f1(wires):
+        qml.CNOT(wires=[wires[numbers[0]], wires[2]])
+        qml.CNOT(wires=[wires[numbers[1]], wires[2]])
+
+    def f2(wires):
+        qml.CNOT(wires=[wires[numbers[2]], wires[2]])
+        qml.CNOT(wires=[wires[numbers[3]], wires[2]])
+
+    def f3(wires):
+        qml.CNOT(wires=[wires[numbers[4]], wires[2]])
+        qml.CNOT(wires=[wires[numbers[5]], wires[2]])
+        qml.PauliX(wires=wires[2])
+
+    def f4(wires):
+        qml.CNOT(wires=[wires[numbers[6]], wires[2]])
+        qml.CNOT(wires=[wires[numbers[7]], wires[2]])
+        qml.PauliX(wires=wires[2])
+
+    output = deutsch_jozsa([f1, f2, f3, f4])
+    print(f"{output}")
