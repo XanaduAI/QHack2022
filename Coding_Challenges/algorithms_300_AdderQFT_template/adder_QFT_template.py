d345ddbb0c5ba3f614d00ab8b065b5c4cb0cb9e9
xanadu-qubot <xanadu@xanadu.ai>
2022-02-14 13:24:57 +0000

    Copied algorithms_300_AdderQFT problem templates

---
+++ Coding_Challenges/algorithms_300_AdderQFT_template/adder_QFT_template.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+
+def qfunc_adder(m, wires):
+    """Quantum function capable of adding m units to a basic state given as input.
+
+    Args:
+        - m (int): units to add.
+        - wires (list(int)): list of wires in which the function will be executed on.
+    """
+
+    qml.QFT(wires=wires)
+
+    # QHACK #
+
+    # QHACK #
+
+    qml.QFT(wires=wires).inv()
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    m = int(inputs[0])
+    n_wires = int(inputs[1])
+    wires = range(n_wires)
+
+    dev = qml.device("default.qubit", wires=wires, shots=1)
+
+    @qml.qnode(dev)
+    def test_circuit():
+        # Input:  |2^{N-1}>
+        qml.PauliX(wires=0)
+
+        qfunc_adder(m, wires)
+        return qml.sample()
+
+    output = test_circuit()
+    print(*output, sep=",")
