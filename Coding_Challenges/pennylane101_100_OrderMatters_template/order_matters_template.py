268612d05d1bde0c90959bc35e500676594625d5
Vaishnavi Markunde <79241223+Vaishnavi-Markunde@users.noreply.github.com>
2022-02-15 20:44:33 +0530

    Update order_matters_template.py

--- Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
+++ Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
@@ -6,8 +6,9 @@ from pennylane import numpy as np
 
 
 def compare_circuits(angles):
+   return abs(circuit1(angles[0],angles[1])-circuit2(angles[0],angles[1]))
     """Given two angles, compare two circuit outputs that have their order of operations flipped: RX then RY VERSUS RY then RX.
-
+    
     Args:
         - angles (np.ndarray): Two angles
 
@@ -18,8 +19,24 @@ def compare_circuits(angles):
     # QHACK #
 
     # define a device and quantum functions/circuits here
+dev1 = qml.device("default.qubit", wires=1)
+
+
+@qml.qnode(dev1)
+def circuit1(x,y):
+    qml.RX(x, wires=0)
+    qml.RY(y, wires=0)
+    return qml.expval(qml.PauliX(0))
+
+@qml.qnode(dev1)
+def circuit2(x,y):
+    qml.RY(y, wires=0)
+    qml.RX(x, wires=0)
+    return qml.expval(qml.PauliX(0))
 
     # QHACK #
+
+
 
 
 if __name__ == "__main__":
